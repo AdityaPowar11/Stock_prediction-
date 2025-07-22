f718b06028d6c9d128ff11fc77deb1af6f40279a
AdityaPowar11 <adityapowar501@gmail.com>
2025-07-22 23:47:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -169,7 +169,7 @@ with tab3:
             with st.spinner("Calculating prediction..."):
                 try:
                     prediction = update_and_predict()
-                    prediction = prediction/10
+                    prediction = prediction/7
                     st.success(f"Predicted NIFTY 50 Close: ₹{(prediction  ):,.2f}")
                     
                     # Calculate prediction change
